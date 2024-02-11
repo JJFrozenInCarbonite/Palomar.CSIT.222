28968349e546275efabb81c86f88a1268292d04e
JJ Hoffmann <jonathan.hoffmann@gmail.com>
2024-02-11 09:59:52 -0800

    added screenshots for assignment 1

--- assignment_2/p2_1/p2.1.cpp
+++ assignment_2/p2_1/p2.1.cpp
@@ -27,19 +27,19 @@ int* computeEaster(const int& y)
     c = y % 100;
     d = b / 4;
     e = b % 4;
-    g = (8 * b + 13) / 25;
-    h = (19 * a + b - d - g + 15) % 30;
+    g = ((8 * b) + 13) / 25;
+    h = ((19 * a) + b - d - g + 15) % 30;
     j = c / 4;
     k = c % 4;
     m = (a + 11 * h) / 319;
-    r = (2 * e + 2 * j - k - h + m + 32) % 7;
+    r = ((2 * e) + (2 * j) - k - h + m + 32) % 7;
     n = (h - m + r + 90) / 25;
     p = (h - m + r + n + 19) % 32;
 
     monthDay[0] = n;
     monthDay[1] = p;
     return monthDay;
-} 
+}
 
 /**
  *  Main function.
@@ -55,12 +55,12 @@ int main()
     string months[] = {"January", "February", "March", "April", "May", "June", 
         "July", "August", "September", "October", "November", "December"}; // array of month name strings
         
-    cout << "Please enter a year: " << endl;
+    cout << "Please enter a year: ";
     cin >> year;
 
     int* monthDay = computeEaster(year);
-    cout << "In " << year << ", Easter fell on " << 
-        months[(*monthDay)] << " " << *(monthDay + 1) << "." << endl;
+    cout << "\nEaster " << year << ": " << 
+        months[*monthDay - 1] << " " << *(monthDay + 1) << "." << endl;
 
     return 0;
 }
